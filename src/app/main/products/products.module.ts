a8c6082f5a749e384a0a754736cb4b92328c246a
MelanieOC <mocharan@meraki-s.com>
2020-06-11 13:27:10 -0500

    card

--- src/app/main/products/products.module.ts
+++ src/app/main/products/products.module.ts
@@ -1,7 +1,22 @@
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatChipsModule } from '@angular/material/chips';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+//import { NgxPaginationModule } from 'ngx-pagination';
+//import { LazyLoadImageModule,  intersectionObserverPreset } from 'ng-lazyload-image';
 
 
 
@@ -11,7 +26,23 @@ import { ProductsComponent } from './products.component';
   ],
   imports: [
     CommonModule,
-    ProductsRoutingModule
+    ProductsRoutingModule,
+    CommonModule,
+    ProductsRoutingModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatAutocompleteModule,
+    MatSnackBarModule,
+    MatSelectModule,
+    MatCardModule,
+    MatChipsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatProgressBarModule
   ]
 })
 export class ProductsModule { }
