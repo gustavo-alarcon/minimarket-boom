a790ad2377a37408eeb86cc4e0c51aa575d20f54
Junjiro <jobando@meraki-s.com>
2020-06-16 11:15:37 -0500

    okey

--- src/app/core/models/sale.model.ts
+++ src/app/core/models/sale.model.ts
@@ -1,16 +1,17 @@
 import { User } from 'src/app/core/models/user.model';
+import { Product } from './product.model';
 export class saleStatusOptions {
   requested: 'Solicitado';
   attended: 'Atendido';
-  confirmed: 'Confirmado';        //can be confirmed only when voucher is valid
+  confirmedRequest: 'Solicitud Confirmada';        //can be confirmed only when voucher is valid
+  confirmedVoucher: 'Voucher Confirmado';
+  confirmedDelivery: 'Delivery Confirmado';
   cancelled: 'Anulado'
 }
 
 export interface Sale {
   id: string;
   correlative: string;
-  requestedProducts: SaleRequestedProducts[];
-  status: saleStatusOptions[keyof saleStatusOptions]
   payType?:string,
   document?:string,
   location: {
@@ -23,19 +24,19 @@ export interface Sale {
     reference: string,
     phone:number
   },
+  status: saleStatusOptions[keyof saleStatusOptions]
+  requestedProducts: SaleRequestedProducts[];
   deliveryDate: Date,
   deliveryFinishedDate?: Date,
   createdAt: Date,
   user:string,
   total: number,
   deliveryPrice: number,
-  voucherPhoto?:string,
-  voucherPath?:string
+  voucherPhoto?:string[],
+  voucherPath?:string[]
 }
 
 export interface SaleRequestedProducts {
-  id: string;       //product id
+  product: Product;       
   quantity: number;
-                    //price should be given by product price???
-                    //va a haber ofertas?
 }
